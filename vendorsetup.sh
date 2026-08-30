948075af34203f9b3797b8c4113924e75921466d
MrGadget84 <superhyper52@gmail.com>
2026-08-30 13:27:45 +0400

    ofox

---
+++ vendorsetup.sh
@@ -0,0 +1,88 @@
+#
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2020-2021 The OrangeFox Recovery Project
+#
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+#
+# 	Please maintain this if you use this script or any part of it
+#
+FDEVICE="a32"
+#set -o xtrace
+
+fox_get_target_device() {
+local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
+   if [ -n "$chkdev" ]; then
+      FOX_BUILD_DEVICE="$FDEVICE"
+   else
+      chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
+      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+   fi
+}
+
+if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
+   fox_get_target_device
+fi
+
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+	export TW_DEFAULT_LANGUAGE="ru"
+	export ALLOW_MISSING_DEPENDENCIES=true
+ 	export LC_ALL="C"
+		
+    # R12.1 Settings
+	export FOX_MAINTAINER_PATCH_VERSION="0"
+	export OF_MAINTAINER="MrGadget84"
+
+	export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
+	#export FOX_VANILLA_BUILD=1
+	export OF_NO_SAMSUNG_SPECIAL=0
+	export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/by-name/recovery"
+
+	# Extra build vars
+	export FOX_USE_NANO_EDITOR=1
+ 	export FOX_DELETE_MAGISK_ADDON=1
+	export FOX_DELETE_AROMAFM=1
+	export OF_FL_PATH1="/sys/class/backlight/panel/brightness"
+	export OF_FL_PATH2=""
+	export OF_FLASHLIGHT_ENABLE=1
+	export OF_HIDE_NOTCH=1
+	export OF_CLOCK_POS=1
+	export OF_ALLOW_DISABLE_NAVBAR=0
+	export OF_USE_SYSTEM_FINGERPRINT=1
+	export OF_USE_TWRP_SAR_DETECT=1
+	export FOX_ENABLE_APP_MANAGER=1
+	export FOX_USE_BASH_SHELL=1
+	export FOX_ASH_IS_BASH=1
+	export FOX_USE_TAR_BINARY=1
+	export FOX_USE_SED_BINARY=1
+	export FOX_USE_XZ_UTILS=1
+	export OF_USE_GREEN_LED=0
+	export OF_SUPPORT_PRE_FLASH_SCRIPT=1
+ 	export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"; # Tuesday, January 1, 2019 12:00:00 AM GMT+00:00
+  	export FOX_USE_DATA_RECOVERY_FOR_SETTINGS=1
+
+	# maximum permissible splash image size (in kilobytes); do *NOT* increase!
+	export OF_SPLASH_MAX_SIZE=104
+
+	# run a process after formatting data to work-around MTP issues
+	export OF_RUN_POST_FORMAT_PROCESS=1
+
+	# let's see what are our build VARs
+ 		if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+		export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+		export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+		export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+		export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+		fi
+fi
+#
